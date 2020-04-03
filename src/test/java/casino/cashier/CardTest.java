ad1db10073496ae6281f435b00959d1d898d6d39
Kerz <k.coipel@student.fontys.nl>
2020-04-03 15:52:55 +0200

    addBetID test

---
+++ src/test/java/casino/cashier/CardTest.java
@@ -0,0 +1,23 @@
+package casino.cashier;
+
+import casino.bet.BetID;
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+
+public class CardTest {
+
+    @Test
+    public void addBetID_ValidBetID_NumberOfBetIDsIsOne() {
+        // Arrange
+        Card card = new Card();
+        BetID betID = mock(BetID.class);
+        int AMOUNTOFBETS = 1;
+
+        // Act
+        card.addBetID(betID);
+
+        // Assert
+        assertThat(AMOUNTOFBETS, card.getNumberOfBetIDs());
+    }
+}
