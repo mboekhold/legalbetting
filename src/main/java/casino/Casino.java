805c8853b53fec3fd42039832e714d3b34e0bb77
Miguel Boekhold <m.boekhold@student.fontys.nl>
2020-04-03 20:55:35 +0200

    test retrieve game by name test

--- src/main/java/casino/Casino.java
+++ src/main/java/casino/Casino.java
@@ -4,19 +4,26 @@ import casino.bet.Bet;
 import casino.cashier.IPlayerCard;
 import casino.game.IGame;
 
+import java.util.HashMap;
+
 
 public class Casino implements ICasino {
 
 
+    private HashMap<String, IGame> games;
+
+    public Casino() {
+        this.games = new HashMap<>();
+    }
 
     @Override
     public void addGame(String gameName, IGame gameToAdd) {
-
+        games.put(gameName, gameToAdd);
     }
 
     @Override
     public IGame getGame(String name) {
-        return null;
+        return games.get(name);
     }
 
     @Override
