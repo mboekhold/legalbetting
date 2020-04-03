2d7a515104e07c6b579b34d6aeb9371ecf5872e8
Kerz <k.coipel@student.fontys.nl>
2020-04-03 16:22:29 +0200

    gaming machine constructor test

---
+++ src/test/java/casino/gamingmachine/GamingMachineTest.java
@@ -0,0 +1,22 @@
+package casino.gamingmachine;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.mock;
+
+public class GamingMachineTest {
+
+    @Test
+    public void gamingMachineConstructor_GameIsSet() {
+        // Arrange
+        Game game = mock(Game.class);
+
+        // Act
+        GamingMachine gamingMachine = new GamingMachine(game);
+
+        // Assert
+        assertThat(game, is(gamingMachine.getGame()));
+    }
+}
